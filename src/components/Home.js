f255ebe2c2b72daf9306dc3b1d7c15eab4cb7d73
aadarshv9 <aadarshv9@gmail.com>
2020-08-30 16:43:46 +0530

    optimized the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,50 +3,16 @@ import { connect } from 'react-redux';
 
 class Home extends React.Component {
   render() {
-    console.log('Home Position : ', this.props.homeList);
-    const { homePosition } = this.props.homeList;
+    const { homePosition, homeList } = this.props.home;
     return (
       <div id="screenList-body">
         <div id="screenList">
-          {homePosition === 0 ? (
-            <div
-              className="screenListItem"
-              style={{ backgroundColor: '#7575e2', color: 'white' }}
-            >
-              Now Playing
-            </div>
-          ) : (
-            <div className="screenListItem">Now Playing</div>
-          )}
-          {homePosition === 1 ? (
-            <div
-              className="screenListItem"
-              style={{ backgroundColor: '#7575e2', color: 'white' }}
-            >
-              Music
-            </div>
-          ) : (
-            <div className="screenListItem">Music</div>
-          )}
-          {homePosition === 2 ? (
-            <div
-              className="screenListItem"
-              style={{ backgroundColor: '#7575e2', color: 'white' }}
-            >
-              Game
-            </div>
-          ) : (
-            <div className="screenListItem">Game</div>
-          )}
-          {homePosition === 3 ? (
-            <div
-              className="screenListItem"
-              style={{ backgroundColor: '#7575e2', color: 'white' }}
-            >
-              Game
-            </div>
-          ) : (
-            <div className="screenListItem">Game</div>
+          {homeList.map((item, index) =>
+            index === homePosition ? (
+              <div className="screenListItem activeListItem">{item}</div>
+            ) : (
+              <div className="screenListItem">{item}</div>
+            )
           )}
         </div>
 
@@ -58,7 +24,7 @@ class Home extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    homeList: state.homeList,
+    home: state.home,
   };
 }
 
--- src/reducers/home.js
+++ src/reducers/home.js
@@ -2,20 +2,20 @@ import { INCREMENT_HOME_POSITION, DECREMENT_HOME_POSITION } from '../actions';
 
 const initialScreenListState = {
   homePosition: 0,
+  homeList: ['Now Playing', 'Music', 'Games', 'Settings'],
 };
 
-export default function homeList(state = initialScreenListState, action) {
-  console.log('In reducer ', action.type);
+export default function home(state = initialScreenListState, action) {
   switch (action.type) {
     case INCREMENT_HOME_POSITION:
       return {
         ...state,
-        homePosition: (state.homePosition + 1) % 4,
+        homePosition: (state.homePosition + 1) % state.homeList.length,
       };
     case DECREMENT_HOME_POSITION:
       return {
         ...state,
-        homePosition: (state.homePosition - 1 + 4) % 4,
+        homePosition: (state.homePosition - 1 + 4) % state.homeList.length,
       };
     default:
       return state;
