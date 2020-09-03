46e90c78641011493dc2ce4cadd1820f14557606
unknown <bhavyasapra96@gmail.com>
2020-09-03 19:54:04 +0530

    Updates

---
+++ precache-manifest.76355263de983101aeef6b72a96e322c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09213ed815844eb17b79baf7cfc5a037",
+    "url": "/virtual-ipod/index.html"
+  },
+  {
+    "revision": "8cb92bdd40ad7ff8b8b8",
+    "url": "/virtual-ipod/static/css/main.22ef2942.chunk.css"
+  },
+  {
+    "revision": "7a44a4ccd1156b315508",
+    "url": "/virtual-ipod/static/js/2.b25f2afd.chunk.js"
+  },
+  {
+    "revision": "0395921f8b56d12a4870f8ea70bf3c15",
+    "url": "/virtual-ipod/static/js/2.b25f2afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb92bdd40ad7ff8b8b8",
+    "url": "/virtual-ipod/static/js/main.17da98da.chunk.js"
+  },
+  {
+    "revision": "bdce96ae3de902fdd013",
+    "url": "/virtual-ipod/static/js/runtime-main.923cf9ed.js"
+  },
+  {
+    "revision": "22b7a8a6ce6b0a11cf5fd35171cff945",
+    "url": "/virtual-ipod/static/media/Despacito.22b7a8a6.png"
+  },
+  {
+    "revision": "ae4c4364644f61500feaddfa55dd7707",
+    "url": "/virtual-ipod/static/media/Despacito.ae4c4364.mp3"
+  },
+  {
+    "revision": "9786544aac0ce0bc58107367b61c631a",
+    "url": "/virtual-ipod/static/media/Galaxy.9786544a.png"
+  },
+  {
+    "revision": "21f11c2634a7fbd6abb7a03aaca1bc63",
+    "url": "/virtual-ipod/static/media/Infra.21f11c26.png"
+  },
+  {
+    "revision": "50c81fa6d4397e5bdf2f0245c37e9276",
+    "url": "/virtual-ipod/static/media/Landscape.50c81fa6.png"
+  },
+  {
+    "revision": "38ff811de026c0c59a99391ca050e77e",
+    "url": "/virtual-ipod/static/media/Laung Gawacha.38ff811d.png"
+  },
+  {
+    "revision": "936d58ce4de34762648806a2f8f0068c",
+    "url": "/virtual-ipod/static/media/Laung Gawacha.936d58ce.mp3"
+  },
+  {
+    "revision": "b4bf373fb1ced3a3a10fa40cec589a63",
+    "url": "/virtual-ipod/static/media/Music.b4bf373f.jpg"
+  },
+  {
+    "revision": "da5761c0c477fa2c9b09024face352fb",
+    "url": "/virtual-ipod/static/media/NowPlaying.da5761c0.jpg"
+  },
+  {
+    "revision": "9d00c838d3dd6aac8a8fedc43505066c",
+    "url": "/virtual-ipod/static/media/Settings.9d00c838.jpg"
+  },
+  {
+    "revision": "209b654fa7ad037be1d65464c538a650",
+    "url": "/virtual-ipod/static/media/Shape of You.209b654f.mp3"
+  },
+  {
+    "revision": "f34a00dd8be301f69d49426b555ad5d4",
+    "url": "/virtual-ipod/static/media/Videos.f34a00dd.jpg"
+  }
+]);
